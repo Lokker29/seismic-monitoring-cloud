fa6d3803a33ba162983d14200321ce5aa7a0d12b
Lokker <bohdan.bilokon@nure.ua>
2020-04-19 16:57:55 +0300

    update configs

--- apps/main.py
+++ apps/main.py
@@ -1,16 +1,18 @@
 from pymongo import MongoClient
 
-from config import DATABASE_NAME, MONGO_DB_URL, BASE_SEISMIC_COLLECTION
+from config import DATABASE_NAME, BASE_SEISMIC_COLLECTION, MONGO_DB_USER, MONGO_DB_PASSWORD, MONGO_DB_HOST
 
 if __name__ == '__main__':
-    client = MongoClient(MONGO_DB_URL)
+    client = MongoClient(
+        host=MONGO_DB_HOST,
+        username=MONGO_DB_USER,
+        password=MONGO_DB_PASSWORD,
+    )
 
     db = client[DATABASE_NAME]
-    # db.authenticate('root', '23052019')
 
     collection = db[BASE_SEISMIC_COLLECTION]
 
     print("Start")
     first = collection.insert_one({'a': 1}).inserted_id
-    print(first)
     print("Done")
