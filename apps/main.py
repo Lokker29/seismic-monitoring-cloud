9f73d1d9b907a8b866dde7837e88309b43e41ed2
Lokker <bohdan.bilokon@nure.ua>
2020-05-03 21:28:56 +0300

    add parser

--- apps/main.py
+++ apps/main.py
@@ -1,9 +1,20 @@
 from config import DATABASE_NAME, BASE_SEISMIC_COLLECTION
 from db import DBClient
 from config import MONGO_DB_HOST, MONGO_DB_USER, MONGO_DB_PASSWORD
+from parser import Parser
 
 
-if __name__ == '__main__':
+def main():
     client = DBClient()
 
     client.create_db(MONGO_DB_HOST, MONGO_DB_USER, MONGO_DB_PASSWORD)
+
+    parser = Parser()
+
+    info = parser.get_information_today()
+
+    client.add_to_collection(DATABASE_NAME, BASE_SEISMIC_COLLECTION, data=info)
+
+
+if __name__ == '__main__':
+    main()
---
+++ apps/parser.py
@@ -0,0 +1,98 @@
+import json
+import pandas as pd
+from datetime import datetime, date
+
+import requests
+
+from config import PARSER_URL
+
+
+class Parser:
+	base_url = PARSER_URL
+
+	COUNT = 'count'
+	QUERY = 'query'
+
+	def _send_request(self, url):
+		response = requests.get(url=url)
+		return response
+
+	def get_count(self, start=None, end=None, format='geojson'):
+		"""
+		param start: str in format 'yyyy-mm-dd'
+		param end: str in format 'yyyy-mm-dd'
+		"""
+		method = self.COUNT
+
+		parameters = []
+
+		if start is not None:
+			start = start.strftime('%Y-%m-%d') if isinstance(start, date) else start
+			parameters.append('starttime=' + start)
+		if end is not None:
+			end = end.strftime('%Y-%m-%d') if isinstance(end, date) else end
+			parameters.append('endtime=' + end)
+		if format is not None:
+			parameters.append('format=' + format)
+
+		parameters = '&'.join(parameters)
+
+		request_url = self.base_url + method + '?' + parameters
+
+		response = self._send_request(request_url)
+
+		data = json.loads(response.text)
+
+		return data['count']
+
+	def get_count_today(self):
+		today = datetime.now().date()
+
+		return self.get_count(start=today)
+
+	def get_information(self, start=None, end=None, format='geojson'):
+		"""
+		param start: str in format 'yyyy-mm-dd'
+		param end: str in format 'yyyy-mm-dd'
+		"""
+		method = self.QUERY
+
+		parameters = []
+
+		if start is not None:
+			start = start.strftime('%Y-%m-%d') if isinstance(start, date) else start
+			parameters.append('starttime=' + start)
+		if end is not None:
+			end = end.strftime('%Y-%m-%d') if isinstance(end, date) else end
+			parameters.append('endtime=' + end)
+		if format is not None:
+			parameters.append('format=' + format)
+
+		parameters = '&'.join(parameters)
+
+		request_url = self.base_url + method + '?' + parameters
+
+		response = self._send_request(request_url)
+
+		data = json.loads(response.text)
+
+		pandasDF = pd.DataFrame.from_dict([{
+			'coord': feature['geometry']['coordinates'],
+			'mag': feature['properties']['mag'],
+			'place': feature['properties']['place'],
+			'time': feature['properties']['time'],
+			'title': feature['properties']['title'],
+			'dmin': feature['properties']['dmin'],
+			'nst': feature['properties']['nst'],
+			'rms': feature['properties']['rms'],
+			'gap': feature['properties']['gap'],
+			'sig': feature['properties']['sig'],
+		} for feature in data['features']])
+
+		pandasDF.dropna(inplace=True)
+		return list(pandasDF.T.to_dict().values())
+
+	def get_information_today(self):
+		today = datetime.now().date()
+
+		return self.get_information(start=today)
